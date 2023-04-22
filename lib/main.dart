fca209085cb198f15f64c5785c38d979fb0d982d
AbuHebil <Mahmoudabohebil@gmail.com>
2023-04-22 16:38:35 +0200

    The Beginnig Of CGPA App.

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
