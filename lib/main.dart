041e5153c87b45c3a731213edc7804728fbb17db
AbuHebil <Mahmoudabohebil@gmail.com>
2023-04-23 15:54:52 +0200

    Setup SignUP Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/welcome.dart';
+import 'pages/signUp.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WelcomePage(),
+      home: SignUpPage(),
     );
   }
 }
---
+++ lib/pages/signUp.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xffb8c8d1),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Color(0xffb8c8d1),
+          body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
