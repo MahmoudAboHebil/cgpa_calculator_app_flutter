1d71845f98f5d70302c56d142f3744c5a919b853
AbuHebil <Mahmoudabohebil@gmail.com>
2023-04-25 12:53:39 +0200

    .

--- lib/pages/signUp.dart
+++ lib/pages/signUp.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:email_validator/email_validator.dart';
-import 'package:cgp_calculator/signin.dart';
+import 'package:cgp_calculator/pages/signin.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
