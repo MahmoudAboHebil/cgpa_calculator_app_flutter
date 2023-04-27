81e4000827855a4ad026231d7eb1a4444346f364
AbuHebil <Mahmoudabohebil@gmail.com>
2023-04-28 01:47:31 +0200

    setUp the validation  of Course component.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:dropdown_button2/src/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:percent_indicator/percent_indicator.dart';
-import 'package:cgp_calculator/test.dart';
 import 'package:provider/provider.dart';
 import 'package:cgp_calculator/providerBrain.dart';
 
@@ -241,8 +240,76 @@ class Course extends StatefulWidget {
 }
 
 class _CourseState extends State<Course> {
+  final TextEditingController _controller_Name = TextEditingController();
+  final TextEditingController _controller_Credit = TextEditingController();
+  String? selectedValue;
+  bool selectedValueIsNull = false;
+  final List<String> items = [
+    'A+',
+    'b+',
+    'c+',
+    'd+',
+    'e+',
+    'f+',
+    'g',
+    'k+',
+  ];
+  String? get _errorCredit {
+    var name = _controller_Name.text;
+    var credit = _controller_Credit.text;
+    if ((name.isNotEmpty && name.trim().isNotEmpty)) {
+      if (credit.isEmpty || credit.trim().isEmpty) {
+        return '';
+      }
+    } else if ((credit.isEmpty || credit.trim().isEmpty) &&
+        selectedValue != null) {
+      return '';
+    }
+
+    return null;
+  }
+
+  String? get _errorName {
+    var name = _controller_Name.text;
+    var credit = _controller_Credit.text;
+    if (credit.isNotEmpty && credit.trim().isNotEmpty) {
+      if (name.isEmpty || name.trim().isEmpty) {
+        return '';
+      }
+    } else if ((name.isEmpty || name.trim().isEmpty) && selectedValue != null) {
+      return '';
+    }
+
+    return null;
+  }
+
+  void errorGrade() {
+    var name = _controller_Name.text;
+    var credit = _controller_Credit.text;
+    if ((name.isNotEmpty && name.trim().isNotEmpty) ||
+        (credit.isNotEmpty && credit.trim().isNotEmpty)) {
+      if (selectedValue == null) {
+        setState(() {
+          selectedValueIsNull = true;
+          print('############# red ###############');
+        });
+      } else {
+        setState(() {
+          selectedValueIsNull = false;
+          print('############# white ###############');
+        });
+      }
+    } else {
+      setState(() {
+        selectedValueIsNull = false;
+        print('############# white ###############');
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    errorGrade();
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 15),
       child: Row(
@@ -252,14 +319,18 @@ class _CourseState extends State<Course> {
           Container(
             width: 125,
             child: TextField(
+              controller: _controller_Name,
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 18, color: Color(0xff004d60)),
               onChanged: (value) {
                 Provider.of<MyData>(context, listen: false).change(true);
-                print('##############################################');
-                print(Provider.of<MyData>(context, listen: false).isChanged);
+                setState(() {
+                  errorGrade();
+                  selectedValueIsNull;
+                });
               },
               decoration: InputDecoration(
+                errorText: _errorName,
                 hintText: 'Enter Course',
                 hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
                 enabledBorder: UnderlineInputBorder(
@@ -276,10 +347,15 @@ class _CourseState extends State<Course> {
           Container(
             width: 60,
             child: TextField(
+              controller: _controller_Credit,
               textAlign: TextAlign.center,
               keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {});
+              },
               style: TextStyle(fontSize: 18, color: Color(0xff4562a7)),
               decoration: InputDecoration(
+                errorText: _errorCredit,
                 hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
                 enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(color: Colors.white),
@@ -292,7 +368,108 @@ class _CourseState extends State<Course> {
               ),
             ),
           ),
-          DropMenuWithIcon(),
+          DropdownButtonHideUnderline(
+            child: DropdownButton2(
+              onMenuStateChange: (value) {
+                errorGrade();
+              },
+              customButton: Container(
+                height: 40,
+                width: 120,
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border(
+                      bottom: BorderSide(
+                          color: selectedValueIsNull
+                              ? Color(0xffce2029)
+                              : Colors.white,
+                          width: 1)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    selectedValue == null
+                        ? Text(
+                            '',
+                          )
+                        : Text(
+                            '$selectedValue',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Color(0xff4562a7),
+                            ),
+                          ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                        size: 35,
+                        color: Color(0xff4562a7),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              items: items
+                  .map((item) => DropdownMenuItem<String>(
+                        value: item,
+                        child: Center(
+                          child: Container(
+                            alignment: Alignment.center,
+                            width: 45,
+                            height: 80,
+                            decoration: BoxDecoration(
+                                border: Border(
+                              bottom: BorderSide(color: Colors.white, width: 1),
+                            )),
+                            child: Text(
+                              item,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                color: Color(0xff4562a7),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+              value: selectedValue,
+              onChanged: (value) {
+                setState(() {
+                  selectedValue = value as String;
+                  errorGrade();
+                });
+              },
+              dropdownStyleData: DropdownStyleData(
+                maxHeight: 200,
+                width: 70,
+                padding: null,
+                elevation: 2,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      bottomRight: Radius.circular(10)),
+                  color: Color(0xffb8c8d1),
+                  // boxShadow: [
+                  //   BoxShadow(color: Colors.white, blurRadius: 5, spreadRadius: 0.2)
+                  // ],
+                ),
+                offset: const Offset(20, 0),
+                scrollbarTheme: ScrollbarThemeData(
+                  radius: const Radius.circular(40),
+                  thickness: MaterialStateProperty.all(0),
+                  thumbVisibility: MaterialStateProperty.all(false),
+                ),
+              ),
+              // menuItemStyleData: const MenuItemStyleData(
+              //   height: 40,
+              //   padding: EdgeInsets.only(left: 14, right: 14),
+              // ),
+            ),
+          ),
         ],
       ),
     );
--- lib/test.dart
+++ lib/test.dart
@@ -1,134 +1,31 @@
-import 'package:dropdown_button2/src/dropdown_button2.dart';
-import 'package:flutter/material.dart';
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xffb8c8d1),
-    );
-  }
-}
-
-class DropMenuWithIcon extends StatefulWidget {
-  @override
-  State<DropMenuWithIcon> createState() => _DropMenuWithIconState();
-}
-
-class _DropMenuWithIconState extends State<DropMenuWithIcon> {
-  String? selectedValue;
-  final List<String> items = [
-    'A+',
-    'b+',
-    'c+',
-    'd+',
-    'e+',
-    'f+',
-    'g',
-    'k+',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButtonHideUnderline(
-      child: DropdownButton2(
-        customButton: Container(
-          height: 40,
-          width: 120,
-          decoration: BoxDecoration(
-            color: Colors.transparent,
-            border: Border(bottom: BorderSide(color: Colors.white, width: 1)),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              selectedValue == null
-                  ? Text(
-                      '',
-                    )
-                  : Text(
-                      '$selectedValue',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Color(0xff4562a7),
-                      ),
-                    ),
-              SizedBox(
-                width: 10,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Icon(
-                  Icons.keyboard_arrow_down_rounded,
-                  size: 35,
-                  color: Color(0xff4562a7),
-                ),
-              )
-            ],
-          ),
-        ),
-        items: items
-            .map((item) => DropdownMenuItem<String>(
-                  value: item,
-                  child: Center(
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 45,
-                      height: 80,
-                      decoration: BoxDecoration(
-                          border: Border(
-                        bottom: BorderSide(color: Colors.white, width: 1),
-                      )),
-                      child: Text(
-                        item,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          color: Color(0xff4562a7),
-                        ),
-                      ),
-                    ),
-                  ),
-                ))
-            .toList(),
-        value: selectedValue,
-        onChanged: (value) {
-          setState(() {
-            selectedValue = value as String;
-          });
-        },
-        dropdownStyleData: DropdownStyleData(
-          maxHeight: 200,
-          width: 70,
-          padding: null,
-          elevation: 2,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10)),
-            color: Color(0xffb8c8d1),
-            // boxShadow: [
-            //   BoxShadow(color: Colors.white, blurRadius: 5, spreadRadius: 0.2)
-            // ],
-          ),
-          offset: const Offset(20, 0),
-          scrollbarTheme: ScrollbarThemeData(
-            radius: const Radius.circular(40),
-            thickness: MaterialStateProperty.all(0),
-            thumbVisibility: MaterialStateProperty.all(false),
-          ),
-        ),
-        // menuItemStyleData: const MenuItemStyleData(
-        //   height: 40,
-        //   padding: EdgeInsets.only(left: 14, right: 14),
-        // ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xffb8c8d1),
+//     );
+//   }
+// }
+//
+// class DropMenuWithIcon extends StatefulWidget {
+//   @override
+//   State<DropMenuWithIcon> createState() => _DropMenuWithIconState();
+// }
+//
+// class _DropMenuWithIconState extends State<DropMenuWithIcon> {
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ;
+//   }
+// }
